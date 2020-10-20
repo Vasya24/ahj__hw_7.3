20e747b34e63dda858376a239ded96a83b39a475
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-10-20 10:13:09 +0300

    Seems like everything is OK

--- src/js/ImageWidget.js
+++ src/js/ImageWidget.js
@@ -3,6 +3,23 @@ export default class ImageWidget {
     this.fileBox = document.querySelector('.overlap');
     this.input = document.getElementById('file');
     this.previewBox = document.querySelector('.preview-box');
+    this.server = 'https://ahj--hw-7-3.herokuapp.com/';
+  }
+
+  init() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', this.server);
+    xhr.addEventListener('load', () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        const files = JSON.parse(xhr.response);
+        files.forEach((el) => {
+          if (el !== '.gitkeep') {
+            this.showPreview(el);
+          }
+        });
+      }
+    });
+    xhr.send();
   }
 
   action() {
@@ -15,7 +32,7 @@ export default class ImageWidget {
       this.fileBox.classList.remove('overlap-dnd');
       if (this.previewBox.children.length === 3) return;
       const { files } = event.dataTransfer;
-      this.showPreview(files);
+      this.post(files);
     });
     this.fileBox.addEventListener('click', () => {
       this.input.dispatchEvent(new MouseEvent('click'));
@@ -23,26 +40,45 @@ export default class ImageWidget {
     this.input.addEventListener('change', (event) => {
       if (this.previewBox.children.length === 3) return;
       const { files } = event.currentTarget;
-      this.showPreview(files);
+      this.post(files);
       this.input.value = '';
     });
   }
 
-  showPreview(files) {
-    files.forEach((el) => {
-      const preview = document.createElement('div');
-      preview.classList.add('preview');
-      const image = document.createElement('img');
-      const deleteButton = document.createElement('div');
-      deleteButton.classList.add('delete-button');
-      image.classList.add('preview-image');
-      image.src = URL.createObjectURL(el);
-      preview.appendChild(image);
-      preview.appendChild(deleteButton);
-      this.previewBox.appendChild(preview);
-      deleteButton.addEventListener('click', () => {
-        deleteButton.parentElement.parentElement.removeChild(deleteButton.parentElement);
+  post(files) {
+    const formData = new FormData();
+    formData.append('file', files[0]);
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', this.server);
+    xhr.addEventListener('load', () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        const link = xhr.response;
+        this.showPreview(link);
+      }
+    });
+    xhr.send(formData);
+  }
+
+  showPreview(link) {
+    const preview = document.createElement('div');
+    preview.classList.add('preview');
+    const image = document.createElement('img');
+    const deleteButton = document.createElement('div');
+    deleteButton.classList.add('delete-button');
+    image.classList.add('preview-image');
+    image.src = `${this.server}${link}`;
+    preview.appendChild(image);
+    preview.appendChild(deleteButton);
+    this.previewBox.appendChild(preview);
+    deleteButton.addEventListener('click', () => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('DELETE', `${this.server}?${link}`);
+      xhr.addEventListener('load', () => {
+        if (xhr.status === 200) {
+          deleteButton.parentElement.parentElement.removeChild(deleteButton.parentElement);
+        }
       });
+      xhr.send();
     });
   }
 }
